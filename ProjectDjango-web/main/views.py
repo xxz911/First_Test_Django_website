d3bd0ba0eaf9928b5894fee0abf8bbfac0576372
xxz <xxz911@rambler.ru>
2022-09-22 01:10:32 +0300

    Created the admin page, the contacts page, introduced the module and the DATABASE

--- ProjectDjango-web/main/views.py
+++ ProjectDjango-web/main/views.py
@@ -2,7 +2,11 @@ from django.shortcuts import render
  
 
 def index(request):
-   return render(request, "main/index.html")
+   data = {
+      'title': 'Главная страница',
+      }
+      
+   return render(request, "main/index.html", data)
 
 def about(request):
     return render(request, "main/about.html")
--- ProjectDjango-web/web/settings.py
+++ ProjectDjango-web/web/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'main',
+    'news',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -104,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
 
 TIME_ZONE = 'UTC'
 
@@ -118,9 +119,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
+# STATICFILES_DIRS = [
+#     BASE_DIR / "static",
+# ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field

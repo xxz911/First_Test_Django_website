448c24a0d0b21246d13fac31c985f971aa58f394
xxz <xxz911@rambler.ru>
2022-09-23 22:12:29 +0300

    Added dynamically resizable pages

--- ProjectDjango-web/news/views.py
+++ ProjectDjango-web/news/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render, redirect
 from .models import Artiles
 from .forms import ArtilesForm
+from django.views.generic import DetailView
 
 def news_home(request):
     news = Artiles.objects.order_by('-date')
     return render(request, 'news/news_home.html', {'news': news})
+
+class NewsDetailView(DetailView):
+    model = Artiles
+    template_name = 'news/details_new.html'
+    context_object_name = 'artile'
 
 def create(request):
     error = ''
